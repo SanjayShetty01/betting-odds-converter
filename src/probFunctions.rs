38347101a3a8de117d6fc898cd2f2dcc57065561
SanjayShetty01 <shettysanjay01@gmail.com>
2024-08-01 22:38:04 +0530

    adding prob functions

---
+++ src/probFunctions.rs
@@ -0,0 +1,18 @@
+fn round_to_two_decimal_places(float_number :f32) -> f32{
+    (float_number * 100.0).round() / 100.0
+}
+
+pub fn moneyline_prob(moneyline :f32) -> f32 {
+    let probability = match moneyline {
+        moneylineValue if moneylineValue > 0.0 => (100.0 / (moneyline + 100.0)) * 100,
+        _ => ((moneyline.abs()) / (moneyline.abs() + 100)) * 100
+    };
+}
+
+pub fn decimal_prob(decimal: f32) -> f32 {
+    round_to_two_decimal_places((1.0 / decimal) * 100.0)
+}
+
+pub fn fraction_prob(fractions: f32) -> f32 {
+    round_to_two_decimal_places((1.0 / decimal) * 100.0)
+}
