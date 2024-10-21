ca0ecac186624f5d403624381ed2e8b5c46f23ae
shettysanjay01 <shettysanjay01@gmail.com>
2024-10-21 07:11:41 +0530

    implementating fractional prob

--- src/betting_odds_calculator.rs
+++ src/betting_odds_calculator.rs
@@ -46,7 +46,32 @@ pub fn money_prob_calc() {
     }
 }
 
-// Figure out fraction in Rust
 pub fn fraction_prob_calc() {
-    unimplemented!()
+    println!("{}", "Enter the Fractional Odds (e.g., 3/4): ".blue());
+    
+    let mut user_entered_fraction = String::new();
+
+    io::stdin()
+    .read_line(&mut user_entered_fraction)
+    .expect("Failed to read Fractional Odds");
+
+    let input = user_entered_fraction.trim();
+
+    let parts: Vec<&str> = input.split('/').collect();
+
+    if parts.len() == 2 {
+        match (parts[0].trim().parse::<f32>(), parts[1].trim().parse::<f32>()) {
+            (Ok(numerator), Ok(denominator)) if denominator != 0.0 => {
+                let value = numerator / denominator;
+                let prob: f32 = prob_functions::fraction_prob(value);
+                println!("{}", format!("The Implied Probability is {}%", prob).green());
+            }
+            _ => {
+                println!("{}", "Invalid fraction entered. Please try again.".red());
+            }
+        }
+    } else {
+        println!("{}", "Invalid fraction format. Please enter in the form 'numerator/denominator'.".red());
+    }
+
 }
