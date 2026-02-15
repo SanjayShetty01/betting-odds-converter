1ff84fd4faba4c9ee8d2ead2520c27185fda5ee2
SanjayShetty01 <shettysanjay01@gmail.com>
2026-02-15 17:29:02 +0530

    changing the application name

--- src/implied.rs
+++ src/implied.rs
@@ -1,10 +1,10 @@
-use std::io;
-use colored::* ;
 use crate::{prob_functions, utils};
+use colored::*;
+use std::io;
 
 pub fn decimal_prob_calc(wager: f64) {
     println!("{}", "Enter the Decimal Odds: ".blue());
-    
+
     let mut user_entered_decimal_value = String::new();
 
     io::stdin()
@@ -13,11 +13,11 @@ pub fn decimal_prob_calc(wager: f64) {
 
     match user_entered_decimal_value.trim().parse::<f32>() {
         Ok(value) => {
-             println!();
+            println!();
             println!("{}", "Calculating the Values...".green());
-            let prob : f32 = prob_functions::decimal_prob(value);
-            let payout : f64 = prob_functions::calculate_payout(prob, wager);
-            let return_on_bet : f64 = prob_functions::calculate_percentage_return(payout, wager);
+            let prob: f32 = prob_functions::decimal_prob(value);
+            let payout: f64 = prob_functions::calculate_payout(prob, wager);
+            let return_on_bet: f64 = prob_functions::calculate_percentage_return(payout, wager);
 
             utils::display_metrics_table(prob as f64, payout, return_on_bet);
         }
@@ -39,44 +39,50 @@ pub fn money_prob_calc(wager: f64) {
 
     match user_entered_moneyline_value.trim().parse::<f32>() {
         Ok(value) => {
-             println!();
+            println!();
             println!("{}", "Calculating the Values...".green());
-            let prob : f32 = prob_functions::moneyline_prob(value);
-            let payout : f64 = prob_functions::calculate_payout(prob, wager);
-            let return_on_bet : f64 = prob_functions::calculate_percentage_return(payout, wager);
+            let prob: f32 = prob_functions::moneyline_prob(value);
+            let payout: f64 = prob_functions::calculate_payout(prob, wager);
+            let return_on_bet: f64 = prob_functions::calculate_percentage_return(payout, wager);
 
             utils::display_metrics_table(prob as f64, payout, return_on_bet);
         }
 
         Err(_) => {
-            println!("{}", "Please Enter an Integer Value for Moneyline Odds".red())
+            println!(
+                "{}",
+                "Please Enter an Integer Value for Moneyline Odds".red()
+            )
         }
     }
 }
 
 pub fn fraction_prob_calc(wager: f64) {
     println!("{}", "Enter the Fractional Odds (e.g., 3/4): ".blue());
-    
+
     let mut user_entered_fraction = String::new();
 
     io::stdin()
-    .read_line(&mut user_entered_fraction)
-    .expect("Failed to read Fractional Odds");
+        .read_line(&mut user_entered_fraction)
+        .expect("Failed to read Fractional Odds");
 
     let input = user_entered_fraction.trim();
 
     let parts: Vec<&str> = input.split('/').collect();
 
     if parts.len() == 2 {
-        match (parts[0].trim().parse::<f32>(), parts[1].trim().parse::<f32>()) {
+        match (
+            parts[0].trim().parse::<f32>(),
+            parts[1].trim().parse::<f32>(),
+        ) {
             (Ok(numerator), Ok(denominator)) if denominator != 0.0 => {
-                 println!();
+                println!();
                 println!("{}", "Calculating the Values...".green());
                 let value = numerator / denominator;
                 let prob: f32 = prob_functions::fraction_prob(value);
-                let payout : f64 = prob_functions::calculate_payout(prob, wager);
-                let return_on_bet : f64 = prob_functions::calculate_percentage_return(payout, wager);
-    
+                let payout: f64 = prob_functions::calculate_payout(prob, wager);
+                let return_on_bet: f64 = prob_functions::calculate_percentage_return(payout, wager);
+
                 utils::display_metrics_table(prob as f64, payout, return_on_bet);
             }
             _ => {
@@ -84,7 +90,9 @@ pub fn fraction_prob_calc(wager: f64) {
             }
         }
     } else {
-        println!("{}", "Invalid fraction format. Please enter in the form 'numerator/denominator'.".red());
+        println!(
+            "{}",
+            "Invalid fraction format. Please enter in the form 'numerator/denominator'.".red()
+        );
     }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,56 @@
 use colored::*;
-mod betting_odds_calculator;
+mod implied;
 mod prob_functions;
 mod utils;
 use figlet_rs;
 use std::io;
 
 fn main() {
-    let standard_font = figlet_rs::FIGfont::standard()
-                                                        .unwrap();
-    let welcome_note = standard_font
-                                    .convert("Betting Odds Converter");
+    let standard_font = figlet_rs::FIGfont::standard().unwrap();
+    let welcome_note = standard_font.convert("Implied");
 
     assert!(welcome_note.is_some());
     print!("{}", welcome_note.unwrap());
     println!("{}", "Calculates Implied Probability".blue());
-     println!();
+    println!();
 
     let mut wager = utils::get_wager();
 
     let mut exit: bool = false;
 
-    while  !exit {
-        
+    while !exit {
         let mut num = String::new();
         println!();
         utils::display_main_menu();
 
         io::stdin()
-        .read_line(&mut num)
-        .expect("Failed to read the number");
+            .read_line(&mut num)
+            .expect("Failed to read the number");
 
         let parsed_num = num.trim().parse::<i32>();
-        
+
         match parsed_num {
             Ok(value) => {
                 println!();
                 utils::which_calc_decider(value, wager);
-                },
-                Err(_) => {
-                    println!("Invalid number! Please try again.");  
-                }
-            
-            };
+            }
+            Err(_) => {
+                println!("Invalid number! Please try again.");
+            }
+        };
 
         println!();
         println!("Press 'x' to exit, 'c' to change the wager or any other key to continue: ");
 
         let mut final_call = String::new();
-            io::stdin()
-                .read_line(&mut final_call)
-                .expect("Failed to read input");
+        io::stdin()
+            .read_line(&mut final_call)
+            .expect("Failed to read input");
 
         if final_call.trim().eq_ignore_ascii_case("x") {
-            exit = true; 
+            exit = true;
         } else if final_call.trim().eq_ignore_ascii_case("c") {
             wager = utils::get_wager();
         }
-}
+    }
 }
